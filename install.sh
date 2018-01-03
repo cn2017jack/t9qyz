6bf0725b809868978e179ae6ef324b970a517002
cn2017jack <35071909+cn2017jack@users.noreply.github.com>
2018-01-04 00:30:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 export LC_ALL="zh_CN.UTF-8"
 export LANG="zh_CN.UTF-8"
-links="http://qyz.ceojy.org"
+links="http://6666.tn"
 echo "正在启动脚本.........."
 yum -y install httpd php php-mysql php-gd php-imap php-ldap php-odbc php-pear php-xml php-xmlrpc PHP unzip zip screen
 echo "正在关闭防火墙....."
@@ -18,11 +18,11 @@ wget $links/export.zip
 unzip export.zip
 echo "正在安装网站....."
 cd /var/www/html
-wget  https://raw.githubusercontent.com/cn2017jack/t9qyz/master/wz.zip
+wget  $links/wz.zip
 unzip wz.zip
 echo "正在设置启动脚本....."
 cd /root
-wget  https://raw.githubusercontent.com/cn2017jack/t9qyz/master/qd.zip
+wget  $links/qd.zip
 unzip qd.zip
 chmod -R 777 /root
 echo "游戏服务端搭建完成！"
